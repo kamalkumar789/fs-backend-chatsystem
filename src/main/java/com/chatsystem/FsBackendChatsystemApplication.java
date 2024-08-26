180d294e23e88a712c671f24f03591c1191bd54a
kamalkumar789 <kamalkumargit@gmail.com>
2024-08-26 13:45:38 +0500

    Fix: initial code

---
+++ src/main/java/com/chatsystem/FsBackendChatsystemApplication.java
@@ -0,0 +1,13 @@
+package com.chatsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsBackendChatsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsBackendChatsystemApplication.class, args);
+	}
+
+}
