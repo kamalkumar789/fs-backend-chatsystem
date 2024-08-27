8c9f6f18b7411b2196c8c41efe7a1c3f746ce4cf
kamalkumar789 <kamalkumargit@gmail.com>
2024-08-27 14:39:32 +0500

    Feat: User model and socket message sent events

---
+++ src/main/java/com/chatsystem/models/Users.java
@@ -0,0 +1,28 @@
+package com.chatsystem.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Users {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = true)
+    private String lastName;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false, length = 256)
+    private String password;
+
+}
